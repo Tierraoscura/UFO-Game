05a8d1ba18cc84ef73eb3c6c045baf53ff6a5ff3
Carlos Garcia <85864280+Tierraoscura@users.noreply.github.com>
2021-07-04 02:44:16 -0700

    Update UFO functions.h

--- UFO functions.h
+++ UFO functions.h
@@ -2,7 +2,6 @@
 
 void display_misses(int misses);
 
-// Declare functions
 void greet();
 void display_status(std::vector<char> incorrect, std::string answer);
 void end_game(std::string answer, std::string codeword);
